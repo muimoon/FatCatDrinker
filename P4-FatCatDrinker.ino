0c23c3481ac8c348146b355371427274c1e05ed9
muimoon <muimoon@gmail.com>
2018-08-29 22:09:07 +0800

    Update P4-FatCatDrinker.ino

--- P4-FatCatDrinker.ino
+++ P4-FatCatDrinker.ino
@@ -1,15 +1,17 @@
 int inputPin = 4;   //  接超声波的 output 引脚到数字D4脚
 int outputPin = 5;  //  接超声波的 input 引脚到数字D5脚
 int valve = 12;    // water valve output
+int ledpin = 11;
 int sensitive_output = 8;
-int sensitive_input = A0;
+int sensitive_input = 0;
 int sensitive;
-int button=1;
-boolean flag=false;
+int button = 1;
+boolean flag = false;
 void setup()
 { //初始化串口及引脚的输入、输出模式
   Serial.begin(9600);
   pinMode(valve, OUTPUT);
+  pinMode(ledpin, OUTPUT);
   pinMode(inputPin, INPUT);
   pinMode(outputPin, OUTPUT);
   pinMode(sensitive_input, INPUT);
@@ -37,19 +39,24 @@ void loop()
   float temp1 = analogRead(sensitive_input) / 4;
   sensitive = temp1 ;
   Serial.println(sensitive);
-  Serial.println(analogRead(A1));
-  if (analogRead(button)<10) 
+  Serial.println(analogRead(button));
+  if (analogRead(button) < 10)
   {
     digitalWrite(valve, HIGH);
-    flag=!flag;
+    digitalWrite(ledpin, HIGH);
+    flag = !flag;
     delay(100);
   }
   if (distance1 >= sensitive)
   {
-    if(flag!=true){
-      digitalWrite(valve, LOW); //如果距离大于50厘米小灯亮起
+    if (flag != true) {
+      digitalWrite(valve, LOW);
+      digitalWrite(ledpin, LOW); //如果距离大于50厘米小灯亮起
     }
   }
   else
+  {
     digitalWrite(valve, HIGH); //如果距离小于50厘米小灯熄灭
+    digitalWrite(ledpin, HIGH);
+  }
 }
