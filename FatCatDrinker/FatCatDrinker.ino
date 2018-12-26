78ae97166f38f6236bebfcadd5ca66ab75b564db
muimoon <muimoon@gmail.com>
2018-12-26 20:16:04 +0800

    attiny85

--- FatCatDrinker/FatCatDrinker.ino
+++ FatCatDrinker/FatCatDrinker.ino
@@ -5,19 +5,19 @@ Used Pin No.
 A0:Potentiometer;
 A2:control valve;
 */
-int SR04_inputPin = 4; // echo
-int SR04_outputPin = 3; // trig
+// int SR04_inputPin = 4; // echo
+// int SR04_outputPin = 3; // trig
+int cat_check = 2;
 // 
-int ledpin = 10; // working status Shown LED
+int ledpin = 3; // working status Shown LED
 // 
-int sensitive_input = A0; // Potentiometer input to control testing distance
-int sensitive = 15;
 // 
-int valve = A2; // water valve output
-int stop_but = 2; // valve stop working
+int valve = 1; // water valve output
+int stop_but = 4; // valve stop working
 // 
 boolean stop_flag = false;
 // 
+int start_no = 1;
 void blink() // interrupt
 {
 	stop_flag = !stop_flag;
@@ -31,23 +31,18 @@ void setup()
 	digitalWrite(ledpin, LOW);
 	// 
 	pinMode(stop_but, INPUT_PULLUP);
-	// 
-	pinMode(SR04_inputPin, INPUT);
-	pinMode(SR04_outputPin, OUTPUT);
-	// 
-	// 
-	pinMode(sensitive_input, INPUT);
-
-	sensitive = map(analogRead(sensitive_input),1,1023,5,25);//added for last setting record
+	// pinMode(SR04_inputPin, INPUT);
+	// pinMode(SR04_outputPin, OUTPUT);
+	pinMode(cat_check, INPUT);
+	// sensitive = map(analogRead(sensitive_input),1,1023,5,25);
 }
 
 void loop()
-
 {
-	if (digitalRead(stop_but) == LOW) 
+	if (digitalRead(stop_but) == LOW)
 	{
 		blink();
-		delay(200);
+		delay(1000);
 	}
 	if (stop_flag == true)
 	{
@@ -56,27 +51,22 @@ void loop()
 		delay(100);
 		digitalWrite(ledpin, LOW);
 		delay(100);
-		float temp1 = analogRead(sensitive_input);
-		sensitive = map(temp1, 1, 1023, 5, 25);
+		// float temp1 = analogRead(sensitive_input);
+		// sensitive = map(temp1, 1, 1023, 5, 25);
 	}
-	unsigned int x1, x2;
-	digitalWrite(SR04_outputPin, LOW); // 使发出发出超声波信号接口低电平2 μs
-	delayMicroseconds(2);
-	digitalWrite(SR04_outputPin, HIGH); // 使发出发出超声波信号接口高电平10μs,这里是至少10μs
-	delayMicroseconds(10);
-	digitalWrite(SR04_outputPin, LOW); // 保持发出超声波信号接口低电平
-	float distance1 = pulseIn(SR04_inputPin, HIGH); // 读出接收脉冲的时间
-	distance1 = distance1 / 58; // 将脉冲时间转化为距离（单位：厘米）
-	x1 = distance1 * 100.0;
-	distance1 = x1 / 100.0; // 保留两位小数
-	delay(150);
 	// 
-	if (distance1 <= sensitive)
+	if (stop_flag == false)
 	{
-		digitalWrite(valve, HIGH);
-		digitalWrite(ledpin, HIGH);
-		delay(20000); // working delay
-		digitalWrite(valve, LOW);
-		digitalWrite(ledpin, LOW);
+		if (digitalRead(cat_check) == HIGH)
+		{
+			digitalWrite(valve, HIGH);
+			digitalWrite(ledpin, HIGH);
+			delay(20000); // working delay
+		}
+		else
+		{
+			digitalWrite(valve, LOW);
+			digitalWrite(ledpin, LOW);
+		}
 	}
 }
