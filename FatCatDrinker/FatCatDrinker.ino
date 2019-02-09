eaf27f09d8cd87ec7d7e14621e4e5aab0296dd8c
muimoon <muimoon@gmail.com>
2019-02-09 10:25:21 +0800

    upfayrf

--- FatCatDrinker/FatCatDrinker.ino
+++ FatCatDrinker/FatCatDrinker.ino
@@ -1,30 +1,21 @@
-
 int cat_check = 2;
-
-int valve = 1; // water valve output
-
+int valve = 0; // water valve output
 void setup()
 {
 	pinMode(valve, OUTPUT);
-
-	digitalWrite(valve, LOW);
-
+	digitalWrite(valve, HIGH);
 	pinMode(cat_check, INPUT);
-
 }
 
 void loop()
 {
-	if (stop_flag == false)
+	if (digitalRead(cat_check) == HIGH)
 	{
-		if (digitalRead(cat_check) == HIGH)
-		{
-			digitalWrite(valve, HIGH);
-			delay(20000); // working delay
-		}
-		else
-		{
-			digitalWrite(valve, LOW);
-		}
+		digitalWrite(valve, LOW);
+		delay(60000); // working delay
+	}
+	else
+	{
+		digitalWrite(valve, HIGH);
 	}
 }
